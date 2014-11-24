9845230c1a864a71b28fff86c48072fd630fec56
Christian Liesch <liesch@gmx.ch>
2014-11-24 23:03:24 +0100

    add a abstract camera object to be able to use other camera types

---
+++ src/Camera.h
@@ -0,0 +1,32 @@
+//----------------------------------------------------------------------------
+// The MIT License
+// Took the basics from Polycode Examples
+//----------------------------------------------------------------------------
+
+#ifndef SYNTHETICS_CAMERA_H
+#define SYNTHETICS_CAMERA_H
+
+#include <Polycode.h>
+#include "PolycodeView.h"
+#include "bullet/LinearMath/btVector3.h"
+#include "bullet/LinearMath/btMatrix3x3.h"
+#include "bullet/LinearMath/btQuaternion.h"
+
+using namespace Polycode;
+
+namespace synthetics {
+
+#define RADIANS_PER_DEGREE 0.01745329f
+#define CAMERA_STEP_SIZE 5.0f
+
+  class Camera : public EventHandler {
+    public:
+      Camera() : EventHandler() {};
+      virtual ~Camera() {};
+      virtual void updateTarget(Vector3 pos) = 0;
+      virtual void update() = 0;
+  };
+
+}
+
+#endif
