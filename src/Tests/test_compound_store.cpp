762bca0051668c4d03cb7e8b2bd11d29b2c16dd0
ia97lies <liesch@gmx.ch>
2015-01-28 22:26:06 +0100

    Test compound store and compound id

--- src/Tests/test_compound_store.cpp
+++ src/Tests/test_compound_store.cpp
@@ -26,3 +26,21 @@ BOOST_AUTO_TEST_CASE(test_compound_store_get_not_existing_compound) {
   CompoundStore store;
   BOOST_CHECK(store.get(0) == NULL);
 }
+
+BOOST_AUTO_TEST_CASE(test_compound_store_more_compound) {
+  CompoundStore store;
+  Compound compound1;
+  Compound compound2;
+  Compound compound3;
+
+  store.insert(&compound1);
+  store.insert(&compound2);
+  store.insert(&compound3);
+  BOOST_CHECK(compound1.getId() == 0);
+  BOOST_CHECK(compound2.getId() == 1);
+  BOOST_CHECK(compound3.getId() == 2);
+  BOOST_CHECK(store.get(0) == &compound1);
+  BOOST_CHECK(store.get(1) == &compound2);
+  BOOST_CHECK(store.get(2) == &compound3);
+  BOOST_CHECK(store.get(3) == NULL);
+}
